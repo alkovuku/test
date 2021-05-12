59499415001d022ed095db0c63821453e86061e7
alkovuku <80138818+alkovuku@users.noreply.github.com>
2021-05-12 16:49:04 +0900

    Add files via upload

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>@yidld('title')</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+    </head>
+    <body>
+        @section('main')
+        @show
+    </body>
+</html>
