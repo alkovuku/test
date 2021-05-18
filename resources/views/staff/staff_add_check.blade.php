883f667325f734b55c0fcc608009a5bb2350d6e5
alkovuku <alkovuku@gmail.com>
2021-05-18 12:25:24 +0900

    commit

--- resources/views/staff/staff_add_check.blade.php
+++ resources/views/staff/staff_add_check.blade.php
@@ -2,47 +2,26 @@
 @section('title', 'スタッフ追加')
 
 @section('main')
-	<!-- $staff_name=$_POST['name'];
-	$staff_pass=$_POST['pass'];
-	$staff_pass2=$_POST['pass2'];
-
-	$staff_name=htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
-	$staff_pass=htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
-	$staff_pass2=htmlspecialchars($staff_pass2,ENT_QUOTES,'UTF-8'); -->
-
-	@if($request->name=='')
-	{
+	@if($name=='')
 		スタッフ名が入力されていません。<br />
-	}
 	@else
-	{
-		スタッフ名：
-		$staff_name;
-		<br />
-	}
+		スタッフ名：{{$name}}<br />
 	@endif
 
-	@if($staff_pass=='')
-	{
+	@if($pass=='')
 		パスワードが入力されていません。<br />
-	}
 	@endif
 
-	@if($staff_pass!=$staff_pass2)
-	{
+	@if($pass!=$pass2)
 		パスワードが一致しません。<br />
-	}
 	@endif
 
-	@if($staff_name=='' || $staff_pass=='' || $staff_pass!=$staff_pass2)
-	{
+	@if($name=='' || $pass=='' || $pass!=$pass2)
 		<form>
 			<input type="button" onclick="history.back()" value="戻る">
 		</form>
-	}
 	@else
-	{
-		$staff_pass=md5($staff_pass);
+		<!-- $staff_pass=md5($staff_pass); -->
 		<form method="post" action="/staff/staff_add_done">
 			@csrf
 			<input type="hidden" name="name" value="'.$staff_name.'">
@@ -51,6 +30,5 @@
 			<input type="button" onclick="history.back()" value="戻る">
 			<input type="submit" value="ＯＫ">
 		</form>
-	}
 	@endif
 @endsection
